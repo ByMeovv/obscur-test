7c521a7f8cd5a046258d337aa3ffd15696b6304f
ByMeovv <moore.meovv@gmail.com>
2025-07-17 13:01:15 +0300

    somes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath, URL } from 'node:url';
 
+const dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export default async () => {
   const plugins = [
     react(),
@@ -19,26 +21,28 @@ export default async () => {
     plugins,
     resolve: {
       alias: {
-        "@": path.resolve(fileURLToPath(new URL('.', import.meta.url)), "src"),
+        "@": path.resolve(dirname, "src"),
       },
     },
-    root: path.resolve(__dirname, 'client'),  // Установите корень проекта, если необходимо
     build: {
       outDir: "dist",
       emptyOutDir: true,
-      sourcemap: false, // Для production
+      sourcemap: false,
       rollupOptions: {
         output: {
-          manualChunks: undefined, // Это может предотвратить ненужное разделение чанков
+          manualChunks: undefined,
         },
       },
     },
     server: {
+      host: true,
+      port: 5173,
       fs: {
         strict: true,
         deny: ["**/.*"],
       },
     },
-    base: './', // Это важно для корректного поведения в production, если проект не в корне домена
+    base: './', // Это ключевое изменение
+    publicDir: 'public'
   });
 };
